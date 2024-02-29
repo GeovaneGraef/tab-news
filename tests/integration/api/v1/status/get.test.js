50b217c258e3f816721b817ace522b50a0bdc983
GeovaneGraef <geovane.graef@hotmail.com>
2024-02-29 12:06:51 +0000

    URL do GITHUB como teste da status page

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
 test("Get to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  const response = await fetch("https://www.githubstatus.com/");
   expect(response.status).toBe(200);
 });
